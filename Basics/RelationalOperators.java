ada87b2877322a7b38ffb43f501d78091acb91f2
Sayantan Das <sayantan.das.private@gmail.com>
2024-05-27 12:13:46 +0530

    Committing RelationalOperators.java

---
+++ Basics/RelationalOperators.java
@@ -0,0 +1,17 @@
+package Basics;
+
+/**
+ * Relational Operator
+ * >, <, >=, <=, ==, !=
+ * returns true/false
+ */
+public class RelationalOperators {
+    public static void main(String[] args) {
+        System.out.println(1 > 3);   // false
+        System.out.println(2.5 < 2.9);   // true
+        System.out.println(10 >= 20);   // false
+        System.out.println(30 <= 30);   // true
+        System.out.println(3.4 == 34);   // false
+        System.out.println(19 != 23);   // true
+    }
+}
