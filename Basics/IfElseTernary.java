abb0fc43d4db1bb6353c1611575872c04a9094b7
Sayantan Das <sayantan.das.private@gmail.com>
2024-05-27 23:38:26 +0530

    Commiting IfElseTernary.java

---
+++ Basics/IfElseTernary.java
@@ -0,0 +1,47 @@
+package Basics;
+
+public class IfElseTernary {
+    public static void main(String[] args) {
+        boolean yes = true;
+        boolean no = false;
+
+        // If
+        if(yes)    {
+            System.out.println("Yes");
+        }
+
+        if(!no)    {
+            System.out.println("Not No");
+        }
+
+        // If-Else
+        if (yes) {
+            System.out.println("If is executed.");
+        }
+        else   {
+            System.out.println("Else is executed.");
+        }
+
+        // If Else-If Else
+        if (yes && no) {
+            System.out.println("1 == 4");
+        }
+        else if (no) {
+            System.out.println("No");
+        }
+        else if (true) {
+            System.out.println("Yes---");
+        }
+        else {
+            System.out.println("last else.");
+        }
+
+        // Ternary
+        // Who is bigger?
+        int a = 12;
+        int b = 10;
+        // ?:
+        int bigger = a > b ? a : b;
+        System.out.println(bigger + " is bigger between " + a + " and " + b);
+    }
+}
