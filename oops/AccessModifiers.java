bcb0278e67c98f590821d78ca5df218607225647
Sayantan Das <sayantan.das.private@gmail.com>
2024-05-31 07:04:30 +0530

    Committing files for Access Modifiers

---
+++ oops/AccessModifiers.java
@@ -0,0 +1,15 @@
+package oops;
+
+/**
+ * There are 4 types of access modifiers,
+ * 1. public: Accessible from any other class.
+ * 2. protected: Accessible within the same package and by subclasses.
+ * 3. default (no modifier): Accessible only within the same package.
+ * 4. private: Accessible only within the same class.
+ */
+
+public class AccessModifiers {
+    public static void main(String[] args) {
+        
+    }
+}
