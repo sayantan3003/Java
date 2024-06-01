8b1b08a9c4c11ead7903d86504e32de01703a06f
Sayantan Das <sayantan.das.private@gmail.com>
2024-06-01 16:09:18 +0530

    Initial commit for Polymorphism.java

---
+++ oops/Polymorphism.java
@@ -0,0 +1,13 @@
+package oops;
+
+/**
+ * Polymorphism is divided into two parts,
+ * 1. Compile time Polymorphism / Early Binding   --> Method Overloading
+ * 2. Run time Polymorphism / Late Binding   --> Method Overridd ing
+ */
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        
+    }
+}
