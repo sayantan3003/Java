4844a99747c354696b8b1520633bfc116dbe3391
Sayantan Das <sayantan.das.private@gmail.com>
2024-06-01 16:08:49 +0530

    Initial commit for Package.java

---
+++ oops/Package.java
@@ -0,0 +1,15 @@
+// Inside oops package
+package oops;
+
+// Importing java package
+import java.util.ArrayList;
+
+// * means all files not all the folders --> java.* means all files inside java folder
+// By default every java file have java.lang.* imported
+import java.lang.*;
+
+public class Package   {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList<>();
+    }
+}
