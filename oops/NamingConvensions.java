1648ad07d35174c60698d15474bc67c6f2731f2c
Sayantan Das <sayantan.das.private@gmail.com>
2024-06-08 13:45:08 +0530

    Initial Commit for NamingConvensions.java

---
+++ oops/NamingConvensions.java
@@ -0,0 +1,13 @@
+package oops;
+
+/**
+ * Camel casing is used
+ * 
+ * 1. class and interface -> PascalCase -> MyJava, HelloWorld, String, Runnable
+ * 2. variable and method -> camaleCase-> marks, show(), showData(), goToTheGYM()
+ * 3. constants -> ALL_IN_UPPERCASE_DIFFERENCIATE_WITH_SNAKE_CASE -> PI, BRAND, MY_WORLD
+ * 4. constructors -> Human(), MyJava()
+ */
+public class NamingConvensions {
+    
+}
